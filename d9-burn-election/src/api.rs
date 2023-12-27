1d1515ecdc895c3983cfef87d2531226393b9891
tangldev <tech@tangl.dev>
2023-12-27 16:40:50 +0800

    added vec

--- d9-burn-election/src/api.rs
+++ d9-burn-election/src/api.rs
@@ -1,4 +1,5 @@
 use codec::Codec;
+use frame_support::inherent::Vec;
 sp_api::decl_runtime_apis! {
     pub trait D9BurnElectionApi<AccountId> where AccountId: Codec {
         fn get_sorted_candidates() -> Option<Vec<AccountId>>;
