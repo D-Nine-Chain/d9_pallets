27a76d7d004dd720c9dfb0aef7ec6ee3f9f6123f
tangldev <tanglplatform@protonmail.com>
2024-12-23 15:54:08 +0800

    work

--- d9-multi-sig/src/types.rs
+++ d9-multi-sig/src/types.rs
@@ -5,6 +5,7 @@ use frame_support::RuntimeDebugNoBound;
 use frame_support::{inherent::Vec, pallet_prelude::*, BoundedVec};
 use frame_system::pallet_prelude::*;
 use sp_core::blake2_256;
+use sp_core::bounded;
 use sp_runtime::traits::Bounded;
 use sp_runtime::traits::Convert;
 use sp_runtime::traits::TrailingZeroInput;
@@ -23,8 +24,8 @@ use sp_runtime::traits::TrailingZeroInput;
 pub struct MultiSignatureAccount<T: Config> {
     /// the address of this multi signature account
     pub address: T::AccountId,
-    /// the 'admins' of this multi signature account. if None then any of the signatories can propose a transaction
-    pub transaction_proposers: Option<BoundedVec<T::AccountId, T::MaxSignatories>>,
+    /// the 'admins' of this multi signature account. if None then any of the signatories can propose a transaction or cancel an existing transaction pending signatures
+    pub authors: Option<BoundedVec<T::AccountId, T::MaxSignatories>>,
     /// the possible signers for this multi signature account
     pub signatories: BoundedVec<T::AccountId, T::MaxSignatories>,
     pub pending_transaction: Option<BoundedVec<u8, T::MaxTransactionSize>>,
@@ -35,38 +36,77 @@ pub struct MultiSignatureAccount<T: Config> {
 impl<T: Config> MultiSignatureAccount<T> {
     pub fn new(
         mut signatories: BoundedVec<T::AccountId, T::MaxSignatories>,
+        authors_opt: Option<Vec<T::AccountId>>,
         minimum_signatories: u32,
-    ) -> Self {
-        assert!(
-            (1 <= minimum_signatories) && (minimum_signatories <= signatories.len() as u32),
-            "minimum signatories must be between 1 and the number of signatories"
-        );
+    ) -> Result<Self, MultiSigAcctError<T>> {
+        // Ensure 1 <= minimum_signatories <= signatories.len()
+        if minimum_signatories < 1 || minimum_signatories > signatories.len() as u32 {
+            return Err(MultiSigAcctError::MinimumSignatoriesRangeError);
+        }
+
+        // Sort signatories for consistent ordering
         signatories.sort();
         let address = Self::construct_address(&signatories);
-        Self {
+
+        // Convert Option<Vec<...>> -> Option<BoundedVec<...>> or None
+        let authors_opt = match authors_opt {
+            Some(mut authors) => {
+                // If authors is the same size as signatories, force authors to None
+                if authors.len() == signatories.len() {
+                    None
+                } else {
+                    // Sort authors to detect duplicates easily
+                    authors.sort();
+
+                    // Check for duplicates by ensuring no adjacent pair is the same
+                    let no_duplicates = authors.windows(2).all(|pair| pair[0] != pair[1]);
+                    if !no_duplicates {
+                        return Err(MultiSigAcctError::DuplicateAuthors);
+                    }
+
+                    // Check that every author is actually a signatory
+                    for author in &authors {
+                        if !signatories.contains(author) {
+                            return Err(MultiSigAcctError::AccountNotSignatory(author.clone()));
+                        }
+                    }
+                    // Convert Vec -> BoundedVec via `.into()`
+                    let try_into_bounded_vec = BoundedVec::try_from(authors);
+                    if try_into_bounded_vec.is_err() {
+                        return Err(MultiSigAcctError::AtMaxAuthors);
+                    }
+                    Some(try_into_bounded_vec.unwrap())
+                }
+            }
+            None => None,
+        };
+
+        Ok(Self {
             address,
-            transaction_proposers: None,
+            authors: authors_opt,
             signatories,
             pending_transaction: None,
             minimum_signatories,
-        }
+        })
     }
 
-    pub fn add_proposer(
-        &mut self,
-        proposer: &T::AccountId,
-    ) -> Result<(), MultiSignatureAccountError> {
-        self.validate_new_proposer(proposer)?;
-        if let Some(ref mut transaction_proposers) = self.transaction_proposers {
-            let push_result = transaction_proposers.try_push(proposer.clone());
-
-            if push_result.is_err() {
-                return Err(MultiSignatureAccountError::AlreadyAtMaxProposers);
+    pub fn add_authors(&mut self, authors: &[T::AccountId]) -> Result<(), MultiSigAcctError<T>> {
+        self.validate_authors(authors)?;
+        match &mut self.authors {
+            Some(authors_bounded_vec) => {
+                let result = authors_bounded_vec.try_extend(authors.iter().cloned());
+                if result.is_err() {
+                    return Err(MultiSigAcctError::AtMaxAuthors);
+                    //note give this a better error name
+                }
             }
-        } else {
-            let mut proposers = BoundedVec::<T::AccountId, T::MaxSignatories>::new();
-            let _ = proposers.try_push(proposer.clone());
-            self.transaction_proposers = Some(proposers);
+            None => {
+                let bounded_authors_res = BoundedVec::try_from(authors.to_vec());
+                if bounded_authors_res.is_err() {
+                    return Err(MultiSigAcctError::AtMaxAuthors);
+                }
+                self.authors = Some(bounded_authors_res.unwrap());
+            }
         }
         Ok(())
     }
@@ -83,20 +123,20 @@ impl<T: Config> MultiSignatureAccount<T> {
             .expect("infinite length input; no invalid inputs for type; qed")
     }
 
-    fn validate_new_proposer(
-        &self,
-        proposer: &T::AccountId,
-    ) -> Result<(), MultiSignatureAccountError> {
-        if let Some(ref transaction_proposers) = self.transaction_proposers {
-            if transaction_proposers.contains(proposer) {
-                return Err(MultiSignatureAccountError::AccountAlreadyProposer);
+    fn validate_authors(&self, authors: &[T::AccountId]) -> Result<(), MultiSigAcctError<T>> {
+        let current_authors_len = self.authors.as_ref().map(|p| p.len()).unwrap_or(0);
+        let max_new_authors = self.signatories.len().saturating_sub(current_authors_len);
+
+        if authors.len() >= max_new_authors {
+            return Err(MultiSigAcctError::AuthorVecTooLong);
+        }
+        let existing_authors = self.authors.as_ref().map(|p| p.as_slice()).unwrap_or(&[]);
+        for author in authors {
+            if existing_authors.contains(author) {
+                return Err(MultiSigAcctError::AlreadyAuthor(author.clone()));
             }
-            if (transaction_proposers.len() + 1) > self.signatories.len() {
-                return Err(MultiSignatureAccountError::AlreadyAtMaxProposers);
-            }
-            if !self.is_signatory(proposer) {
-                return Err(MultiSignatureAccountError::AccountNotSignatory);
-            }
+            if !self.is_signatory(author) {}
+            return Err(MultiSigAcctError::AccountNotSignatory(author.clone()));
         }
         Ok(())
     }
@@ -112,14 +152,23 @@ impl<T: Config> MultiSignatureAccount<T> {
     TypeInfo,
     MaxEncodedLen,
 )]
-pub enum MultiSignatureAccountError {
-    MultiSignatureAccountError,
-    MultiSignatureAccountAlreadyExists,
-    AccountAlreadyProposer,
-    AccountNotSignatory,
-    AlreadyAtMaxProposers,
-    MultiSignatureAccountNotFound,
-    PendingTransactionAlreadyExists,
+#[scale_info(skip_type_params(T))]
+pub enum MultiSigAcctError<T: Config> {
+    /// minimum signatories must be between 1 and the number of signatories
+    MinimumSignatoriesRangeError,
+    /// duplicates in Authors list
+    DuplicateAuthors,
+    /// a multi sig account with the **exact** same signatories already exists.
+    MultiSigAccountExists(T::AccountId),
+    /// proposed account already in the list
+    AlreadyAuthor(T::AccountId),
+    /// proposer length too long
+    AuthorVecTooLong,
+    /// not part of the signatories of multi sig account so can not be proposer or sign
+    AccountNotSignatory(T::AccountId),
+    AtMaxAuthors,
+    MultiSigAccountNotFound,
+    AlreadyPendingTransaction,
     SignatoriesOutOfOrder,
-    SenderInSignatories,
+    SenderInSignatories(T::AccountId),
 }
